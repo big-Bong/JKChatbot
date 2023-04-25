c9362bc15db20cea2a95f07fc775507733666483
big-Bong <vasuchat@gmail.com>
2023-04-25 21:47:47 +0530

    Langchain and PDF connector added.

--- Chat.py
+++ Chat.py
@@ -1,6 +1,5 @@
 import requests
 from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
 
 from ChatGPTConnect import askgpt
 
@@ -10,18 +9,8 @@ app = Flask(__name__)
 def bot():
     print(request.json)
     query = request.json["query"]
-    resp = MessagingResponse()
-    msg = resp.message()
-    answer, log = askgpt(query)
-    msg.body(answer)
-    return str(resp)
+    answer = askgpt(query)
+    return str(answer)
 
 if __name__ == '__main__':
     app.run(port=4000)
-"""
-query = ""
-while(query!="thank you"):
-    query = input("User: ")
-    answer, log = askgpt(query)
-    print("Bot: "+answer+"\n")
-"""
